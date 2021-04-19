f3980cc0edc65fd8f9899aaba9ae1ef0f708f721
besnn <besniknuro74@gmail.com>
2021-04-19 19:19:46 +0200

    occasiona update

--- win/scripts/batlev.bat
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-@echo off
-printf "Internal External\n"
-powershell -command "(Get-CimInstance Win32_Battery).EstimatedChargeRemaining" | tr \n "\t  "
-=======
-@echo off
-printf "Internal External\n"
-powershell -command "(Get-CimInstance Win32_Battery).EstimatedChargeRemaining" | tr \n \t
->>>>>>> 7a7964ad0300a2585c0bcab9cf9367e6be5b2e81
